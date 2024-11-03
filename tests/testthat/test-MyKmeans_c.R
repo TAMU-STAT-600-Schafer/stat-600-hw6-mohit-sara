be2bac4f4ae23a13db7b60148b0ef4376c56c931
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-03 02:53:12 -0600

    Added a microbenchmark based test on MyKmeans_c function with two different M inputs.

--- tests/testthat/test-MyKmeans_c.R
+++ tests/testthat/test-MyKmeans_c.R
@@ -15,3 +15,13 @@ test_that("Test 1: MyKmeans_c Function works!", {
   expect_equal(length(Y_MyKmeans_WithM4), nrow(X))
   expect_equal(length(unique(Y_MyKmeans_WithM4)), nrow(M4))
 })
+
+test_that("Test 3: MyKmeans_c Function works!", {
+  time <- suppressWarnings(microbenchmark(
+    MyKmeans_c(X, K, M3, numIter),
+    MyKmeans_c(X, K, M4, numIter),
+    times = 10
+  ))
+  expect_equal(length(time$time), 20)
+  expect_equal(dim(time), c(20, 2))
+})
