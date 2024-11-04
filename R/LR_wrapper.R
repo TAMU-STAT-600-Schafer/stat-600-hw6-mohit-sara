283397e7a5e800963c1de64b1583b817c09efb4b
Mohit Chhaparia <mohit_chhaparia@tamu.edu>
2024-11-03 22:23:13 -0600

    Added a check on eta.

--- R/LR_wrapper.R
+++ R/LR_wrapper.R
@@ -34,6 +34,8 @@ LRMultiClass <- function(X, y, numIter = 50, eta = 0.1, lambda = 1, beta_init = 
   # Compatibility checks
   if (!all(X[, 1] == 1)) stop("First column of X must be all ones for intercept.")
   if (nrow(X) != length(y)) stop("Number of rows in X must match length of y.")
+  # Check to ensure eta is not NA or non-numeric
+  if(is.na(eta) | !is.numeric(eta) | length(eta) != 1 | is.infinite(eta)) stop("eta must be a single positive number.")
   if (eta <= 0) stop("Learning rate (eta) must be positive.")
   if (lambda < 0) stop("Regularization parameter (lambda) must be non-negative.")
   
